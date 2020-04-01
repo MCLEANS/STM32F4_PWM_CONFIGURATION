b403723202c7913784a1c155d92c834c389e53cd
MCLEANS <jackagwaya@gmail.com>
2020-04-01 07:17:44 +0300

    Library header file

---
+++ IMPLEMENTATION/src/PWM.h
@@ -0,0 +1,21 @@
+/*
+ * PWM.h
+ *
+ *  Created on: Apr 1, 2020
+ *      Author: MCLEANS
+ */
+
+#ifndef PWM_H_
+#define PWM_H_
+
+namespace custom_libraries {
+
+class PWM {
+public:
+	PWM();
+	~PWM();
+};
+
+} /* namespace custom_libraries */
+
+#endif /* PWM_H_ */
