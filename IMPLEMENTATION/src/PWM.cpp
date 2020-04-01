9a6a250812a4cba07b711af72b492d14a51ce1d5
MCLEANS <jackagwaya@gmail.com>
2020-04-01 21:58:49 +0300

    enable timer RCC in class constructor

--- IMPLEMENTATION/src/PWM.cpp
+++ IMPLEMENTATION/src/PWM.cpp
@@ -15,7 +15,24 @@ PWM::PWM(TIM_TypeDef *TIMER, GPIO_TypeDef *PORT, uint8_t PIN, uint16_t prescaler
 																												prescaler(prescaler),
 																												auto_reload_value(auto_reload_value)
 																													{
+	//Enable Timer RCC
+		if(TIMER == TIM1) RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+		if(TIMER == TIM2) RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+		if(TIMER == TIM3) RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+		if(TIMER == TIM4) RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+		if(TIMER == TIM5) RCC->APB1ENR |= RCC_APB1ENR_TIM5EN;
+		if(TIMER == TIM6) RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+		if(TIMER == TIM7) RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
+		if(TIMER == TIM8) RCC->APB2ENR |= RCC_APB2ENR_TIM8EN;
+		if(TIMER == TIM9) RCC->APB2ENR |= RCC_APB2ENR_TIM9EN;
+		if(TIMER == TIM10) RCC->APB2ENR |= RCC_APB2ENR_TIM10EN;
+		if(TIMER == TIM11) RCC->APB2ENR |= RCC_APB2ENR_TIM11EN;
+		if(TIMER == TIM12) RCC->APB1ENR |= RCC_APB1ENR_TIM12EN;
+		if(TIMER == TIM13) RCC->APB1ENR |= RCC_APB1ENR_TIM13EN;
+		if(TIMER == TIM14) RCC->APB1ENR |= RCC_APB1ENR_TIM14EN;
 
+		TIMER->PSC = this->prescaler;
+		TIMER->ARR = this-> auto_reload_value;
 
 }
 
