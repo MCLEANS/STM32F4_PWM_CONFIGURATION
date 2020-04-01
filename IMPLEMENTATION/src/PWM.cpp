2542f6a89cf4e68a145cba6eccd8ff19cf9ca0a2
MCLEANS <jackagwaya@gmail.com>
2020-04-01 07:37:53 +0300

    constructor initialization list definition

--- IMPLEMENTATION/src/PWM.cpp
+++ IMPLEMENTATION/src/PWM.cpp
@@ -9,8 +9,13 @@
 
 namespace custom_libraries {
 
-PWM::PWM() {
-	// TODO Auto-generated constructor stub
+PWM::PWM(TIM_TypeDef *TIMER, GPIO_TypeDef *PORT, uint8_t PIN, uint16_t prescaler,uint16_t auto_reload_value ):TIMER(TIMER),
+																												PORT(PORT),
+																												PIN(PIN),
+																												prescaler(prescaler),
+																												auto_reload_value(auto_reload_value)
+																													{
+
 
 }
 
