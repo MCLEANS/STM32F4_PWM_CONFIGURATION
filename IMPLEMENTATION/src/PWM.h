0dcb5732dba6b228a2d03bcfa2b40742d69b1107
MCLEANS <jackagwaya@gmail.com>
2020-04-01 07:29:06 +0300

    Declaration of constructor, getters and setters

--- IMPLEMENTATION/src/PWM.h
+++ IMPLEMENTATION/src/PWM.h
@@ -8,12 +8,33 @@
 #ifndef PWM_H_
 #define PWM_H_
 
+#include "stm32f4xx.h"
+
 namespace custom_libraries {
 
 class PWM {
+private:
+	TIM_TypeDef *TIMER;
+	GPIO_TypeDef *PORT;
+	uint8_t PIN;
+
+	uint16_t prescaler;
+	uint16_t auto_reload_value;
+	uint16_t duty_cycle;
+
+private:
 public:
-	PWM();
+public:
+
+public:
+	PWM(TIM_TypeDef *TIMER, GPIO_TypeDef *PORT, uint8_t PIN, uint16_t prescaler = 0, uint16_t auto_reload_value = 0);
 	~PWM();
+	void set_prescaler(uint16_t);
+	uint16_t get_prescaler(void) const;
+	void set_auto_reload_value(uint16_t);
+	uint16_t get_auto_reload_value(void) const;
+	void set_duty_cycle(uint16_t);
+	uint16_t get_duty_cycle(void) const;
 };
 
 } /* namespace custom_libraries */
